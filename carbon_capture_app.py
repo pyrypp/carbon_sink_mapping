6835a85b4cc877cb44cb6f3945b08a2262a80643
pyrypp <120693130+pyrypp@users.noreply.github.com>
2024-11-10 09:11:32 +0200

    update text

--- carbon_capture_app.py
+++ carbon_capture_app.py
@@ -16,6 +16,8 @@ text_col_1, text_col_2 = st.columns(spec=[2.2,3])
 with text_col_1:
     st.write("This is a visualization tool to determine the most efficient locations to plant trees for carbon sequestration (i.e. capturing carbon from the atmosphere). The tool uses land cover data, soil data and temperature data to determine potential locations for new carbon sinks.")
     st.write("The brightness of image pixels indicate the carbon capture potential in that area for the selected tree species. We use an index to estimate the sequestration potential. Dark pixels indicate high potential. The map uses the EPSG:3386 projection. For more technical details go to the slides.")
+    st.write("This platform is demoed for the Finland region. It can be expanded to other regions around the globe.")
+    st.link_button("See the slides", "https://docs.google.com/presentation/d/1hxc4hNrIjpkBPzDgIy5G75mM75H-B_y82ZKY6XuH5vc/edit?usp=sharing")
 
 st.write("---")
 st.subheader("Carbon capture potential by tree species mapped")
@@ -23,12 +25,6 @@ st.subheader("Carbon capture potential by tree species mapped")
 col1, col2 = st.columns(spec=[2,1], vertical_alignment="center")
 
 with col2:
-    # st.write("")
-    # st.write("")
-    # st.write("")
-    # st.write("")
-    # st.write("")
-    # st.write("")
     selected_tree = st.radio(label="Available maps", options=trees)
 
 with col1:
